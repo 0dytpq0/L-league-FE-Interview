ec7f40128425c2a6a7673808ce415f5a958165f7
0dytpq0 <rkfnahs12e@gmail.com>
2025-04-15 17:12:23 +0900

    design : header 반응형 디자인 추가

--- src/app/(beforeLogin)/_component/MainHeader.tsx
+++ src/app/(beforeLogin)/_component/MainHeader.tsx
@@ -10,13 +10,13 @@ interface MainHeaderProps {
 
 export default function MainHeader({ title, onSearchClick }: MainHeaderProps) {
   return (
-    <Header 
-      title={title} 
-      headerClassName="mx-4 py-[14px]"
-      titleClassName="text-brand font-bold text-2xl"
+    <Header
+      title={title}
+      headerClassName="mx-4 tablet:mx-8 py-[14px] tablet:py-[18px]"
+      titleClassName="text-brand font-bold text-2xl tablet:text-3xl"
       rightComponent={
         <div className="flex items-center gap-4">
-          <button 
+          <button
             onClick={onSearchClick}
             className="cursor-pointer"
             aria-label="검색"
@@ -24,14 +24,14 @@ export default function MainHeader({ title, onSearchClick }: MainHeaderProps) {
             <ImageWrapper
               src={"/icon_search.svg"}
               alt="search"
-              containerClassName="w-5 h-5 relative aspect-auto"
+              containerClassName="w-5 h-5 tablet:w-6 tablet:h-6 relative aspect-auto"
               objectFit="cover"
             />
           </button>
           <ImageWrapper
             src={"/icon_bell.svg"}
             alt="bell"
-            containerClassName="w-5 h-[22px] relative aspect-auto"
+            containerClassName="w-6 h-6 tablet:w-7 tablet:h-7 relative aspect-auto"
             objectFit="cover"
             imageClassName="cursor-pointer"
           />
