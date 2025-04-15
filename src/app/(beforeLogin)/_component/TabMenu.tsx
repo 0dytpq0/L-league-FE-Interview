2cfa0dc8ed894912c86e90c38543cc2c6abac594
0dytpq0 <rkfnahs12e@gmail.com>
2025-04-15 17:58:33 +0900

    design : tabmenu 반응형 디자인 추가

--- src/app/(beforeLogin)/_component/TabMenu.tsx
+++ src/app/(beforeLogin)/_component/TabMenu.tsx
@@ -1,25 +1,27 @@
-
-
 interface TabMenuProps {
   tabs: { id: number; name: string }[];
   selectedTab: number;
   onTabChange: (index: number) => void;
 }
 
-export default function TabMenu({ tabs, selectedTab, onTabChange }: TabMenuProps) {
+export default function TabMenu({
+  tabs,
+  selectedTab,
+  onTabChange,
+}: TabMenuProps) {
   return (
-    <div className="w-full px-[18px] py-4 flex justify-between">
+    <div className="w-full px-[18px] py-4 tablet:py-8 tablet:px-8 flex justify-between">
       {tabs.map((tab, index) => (
         <div
           key={index}
           className="relative min-w-[35px] text-center cursor-pointer"
           onClick={() => onTabChange(index)}
         >
-          <span className="text-[15.4px] text-[#737373] font-bold">
+          <span className="text-[15.4px] tablet:text-[17.4px] text-[#737373] font-bold">
             {tab.name}
           </span>
           {selectedTab === index && (
-            <div className="absolute -bottom-[5px] min-w-[35px] w-full h-[5px] bg-brand rounded-xl"></div>
+            <div className="absolute -bottom-[5px] tablet:-bottom-[7px] min-w-[35px] w-full h-[5px] bg-brand rounded-xl"></div>
           )}
         </div>
       ))}
